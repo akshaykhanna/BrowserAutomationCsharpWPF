fb596c1f9510fdec9d36f76082a83e2f19d93b2e
Akshay Khanna <akshay9911102163@gmail.com>
2016-07-07 13:27:52 +0530

    Code to open firefox browser

---
+++ FirefoxAutomation/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace FirefoxAutomation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_open_browser_Click(object sender, RoutedEventArgs e)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.google.co.in";
+        }
+    }
+}
